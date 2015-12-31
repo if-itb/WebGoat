648857899e86def275ec0def59c0a612081a06f4
alifanuraniputri <alifanuraniputri@gmail.com>
2015-12-31 16:22:44 +0700

    images

---
+++ webgoat-container/src/test/java/org/owasp/webgoat/lessons/AbstractLessonTestTest.java
@@ -0,0 +1,14 @@
+package org.owasp.webgoat.lessons;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractLessonTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
